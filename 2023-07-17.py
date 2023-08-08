582b905072c18fd158bc1cd330a81c88552d1a58
Jesper Dramsch <jesper@dramsch.net>
2023-08-08 12:57:40 +0200

    Explode some strings

---
+++ 2023-07-17.py
@@ -0,0 +1,36 @@
+from typing import List
+import re
+from itertools import groupby
+
+
+def explodeString(string: str) -> List[str]:
+    """Explode a string into a list of sorted characters.
+
+    Parameters
+    ----------
+    string : str
+        String to explode.
+
+    Returns
+    -------
+    List[str]
+        Sorted list of characters
+
+    Examples
+    --------
+    >>> explodeString('Ahh, abracadabra!')
+    ['!', ',', 'A', 'aaaaa', 'bb', 'c', 'd', 'hh', 'rr']
+
+    >>> explodeString('\o/\o/')
+    ['//', '\\\\', 'oo']
+    """
+    return [
+        "".join(list(g)).replace("\\\\", "\\")
+        for k, g in groupby(sorted(re.sub(r"\s", "", string)))
+    ]
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
